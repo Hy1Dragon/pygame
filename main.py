ace5d239797ccf3d615020888f6fb415990c9ff4
Hy1Dragon <mihailvasilev6090@gmail.com>
2025-04-10 19:43:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,30 @@
-import pygame
+import pygame #импортируем код в pygame
 
-class Food():
-    def __init__(self,a,b,c):
-        self.image = pygame.image.load(a)# загрузка картинки
-        self.rect = self.image.get_rect()# получение прямоугольника от картинки
-        self.x = b
-        self.y = c
+class Food():#создание класса
+    def __init__(self,a,b,c):#создание конструктора, в нем создается свойства,он вызывается при создании объекта
+        self.img = pygame.image.load(a)#создание картинки,ЭТО СВОЙСТВО
+
+        self.rect = self.img.get_rect()# получение прямоугольника от картинки,ЭТО СВОЙСТО
+        self.rect.x = b#создание координат,ЭТО СВОЙСТВО
+        self.rect.y = c#создание координат,ЭТО СВОЙСТВО
 
     def draw_image(self):#метод отрисовки картинки
-        screen.blit(self.image, (self.x, self.y))
+        screen.blit(self.img, (self.rect.x, self.rect.y))
 
 
-fon = Food('кухня.jpg', 0, 0)
-pygame.init()
-window_size=(931,495)#размеры окна
+fon = Food('кухня.jpg', 0, 0)# создание фона
+plate = Food('plate.png', 360, 450)# создание фона
+pygame.init()#обезательная программа
+window_size=(940,500)#размеры окна
 screen=pygame.display.set_mode(window_size)#сделать экран с размерами
-pygame.display.set_caption("Undertale")
-clock = pygame.time.Clock()#фпс
-
-op = Food ('еда 1.png',0,0)
-po = Food ('еда 3.png',0,0)
-poop = Food('еда4.png',0,0)
+clock = pygame.time.Clock() #фпс
 
 while True:#игровой цикл
-    fon.draw_image()#приминение метода отрисовки картинки к объкеу-картинки
-    op.draw_image()
-    po.draw_image()
-    poop.draw_image()
+    fon.draw_image()#приминение метода отрисовки картинки к объкеу klassa Food (фон)
+    plate.draw_image() #приминение метода отрисовки картинки к объкеу klassa Food (тарелка)
 
     clock.tick(40)#40фпс
     for event in pygame.event.get():#события
         if event.type == pygame.QUIT:#если нажали крест
               pygame.QUIT()# выход из игры
-    pygame.display.update()  # оюновление содержимого экрана
+    pygame.display.update()  # обновление содержимого экрана
