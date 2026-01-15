fe8ad5227e2212fe26ecc178e976baecd4f527b7
Hy1Dragon <mihailvasilev6090@gmail.com>
2026-01-15 20:03:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,64 @@
+
+from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QApplication, QWidget, QPushButton
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QLineEdit
 import random
-def SANIS():
-    a = ['(@_@)','54','456','745','47','45734','9']
-    asgor.setText(random.choice(a))
+
+def add_one():
+    pass
+
 
 app = QApplication([])
 main_win = QWidget()
 main_win.setWindowTitle('Underverse')
+
 main_win.resize(400,200)
 main_win.show()
 asgor=QLabel('(@_@):')
 
-list={'task': 'Купить молоко', 'done': False}
-papirus = QPushButton('Pypsik')
+button = QPushButton("удалить все")
+button2 = QPushButton("удалить выбранную заметку")
+button3 = QPushButton("переместить в сделано")
+button4 = QPushButton("удалить выбранную заметку")
+button5 = QPushButton("добавить")
+
+papirus = QPushButton('добавить')
+winner = QLabel('удалить все')
+winner1 = QLabel('удалить выбранную заметкy')
+winner2 = QLabel('переместить в сделано')
+winner3 = QLabel('переместить в список дел')
+winner4 = QLabel('удалить выбранную заметку')
+winner5 = QLabel('удалить все')
+winner.text()
+sanis = QLineEdit()
 v_line = QVBoxLayout()
-
-v_line.addWidget(asgor,alignment = Qt.AlignCenter)
-
-v_line.addWidget(papirus,alignment = Qt.AlignCenter)
+h_line = QHBoxLayout()
 
 main_win.setLayout(v_line)
 
-papirus.clicked.connect(SANIS)
+h_line.addLayout(v_line)
+v_line.addWidget(asgor,alignment = Qt.AlignCenter)
+v_line.addWidget(asgor,alignment = Qt.AlignCenter)
+v_line.addWidget(button,alignment = Qt.AlignCenter)
+v_line.addWidget(button2,alignment = Qt.AlignCenter)
+v_line.addWidget(button3,alignment = Qt.AlignCenter)
+v_line.addWidget(button4,alignment = Qt.AlignCenter)
+v_line.addWidget(button5,alignment = Qt.AlignCenter)
 
+v_line.addWidget
+
+main_win.setLayout(v_line)
+button.clicked.connect(add_one)
+button2.clicked.connect(add_one)
+button3.clicked.connect(add_one)
+button4.clicked.connect(add_one)
+button5.clicked.connect(add_one)
+sanis.text()
 app.exec_()
+
+
+
+
+
 
